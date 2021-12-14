b46949d6fc8671bec9535d6fdce1ec13301f331c
Chris <christaylor269@gmail.com>
2021-12-14 20:41:06 +0100

    removed package

--- src/pages/HOME/HomePage.jsx
+++ src/pages/HOME/HomePage.jsx
@@ -4,11 +4,6 @@ import Navbar from "../../components/Navbar/Navbar";
 import { useGame } from "../../Context/GameContext";
 import "./HomePage.css";
 
-import { FaReact } from "react-icons/fa";
-import { DiJavascript1 } from "react-icons/di";
-import { DiCss3 } from "react-icons/di";
-import { DiHtml5 } from "react-icons/di";
-
 function HomePage() {
   const { resetGame } = useGame();
 
@@ -24,15 +19,13 @@ function HomePage() {
           {
             title: "JS",
             value: "js",
-            logo: <DiJavascript1 className="icon" />,
           },
           {
             title: "React",
             value: "react",
-            logo: <FaReact className="icon" />,
           },
-          { title: "CSS", value: "css", logo: <DiCss3 className="icon" /> },
-          { title: "HTML", value: "html", logo: <DiHtml5 className="icon" /> },
+          { title: "CSS", value: "css" },
+          { title: "HTML", value: "html" },
         ].map((e) => (
           <Category key={e.value} {...e} className="category" />
         ))}
