f167ca228c09c336ece6245ff6555c5158bbd078
Chris <christaylor269@gmail.com>
2021-12-17 22:23:09 +0100

    styles finished

--- src/pages/PROFILE/Profile.jsx
+++ src/pages/PROFILE/Profile.jsx
@@ -4,47 +4,43 @@ import "./Profile.css";
 import { remove } from "../../services/auth";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../Context/UserContext";
-// import { useEffect } from "react/cjs/react.development";
+import { useEffect } from "react/cjs/react.development";
 
 function Profile({ handleLogout, user }) {
   let navigate = useNavigate();
 
-  const { removeUser } = useUser();
+  const { removeUser, fetchUserData } = useUser();
 
   function deleteAccount() {
     remove(user).then((serverResponse) => {
       removeUser();
       navigate("/auth/login");
-      // if(serverResponse.status == false){
-      // }
     });
   }
 
-  // //! not finished
-  // useEffect(() => {
-  //   fetchUserData();
-  // }, [fetchUserData]);
+  useEffect(() => {
+    fetchUserData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
       <Navbar handleLogout={handleLogout} user={user} />
       <div className="container-profile">
-        <section className="profile-section">
-          {console.log(user)}
-          <h1>USERNAME</h1>
-          <h2>{user.username}</h2>
-          <h1>COUNTRY</h1>
-          <h2>{user.country}</h2>
-          <h1>SCORE</h1>
-          <h2>{user.score}</h2>
-          <button
-            className="button__submit"
-            type="submit"
-            onClick={deleteAccount}
-          >
-            Delete my account
-          </button>
-        </section>
+        {console.log(user)}
+        <h1>USERNAME</h1>
+        <h2>{user.username}</h2>
+        <h1>COUNTRY</h1>
+        <h2>{user.country}</h2>
+        <h1>SCORE</h1>
+        <h2>{user.score}</h2>
+        <button
+          className="button__submit"
+          type="submit"
+          onClick={deleteAccount}
+        >
+          Delete my account
+        </button>
       </div>
     </>
   );
